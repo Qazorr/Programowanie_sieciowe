f2b3429c8bde716dfc4469b8161c5fcb8020611c
Qazorr <kpiatkowski223@gmail.com>
2022-04-07 20:06:12 +0200

    Working whitespace error recognition

--- z5/src/server.c
+++ z5/src/server.c
@@ -10,17 +10,19 @@
 #include <stdarg.h>
 #include <errno.h>
 #include <limits.h>
+#include <ctype.h>
 
 #define BACKLOG_SIZE 8
 #define PORT 2020
-#define BUFFER 4
+#define BUFFER 16
 #define LINE_BUFFER 256
 #define CR 13
 #define LF 10
+#define SPACE 32
 #define ul unsigned long
 #define ERROR_MESSAGE "ERROR"
 
-// #define DEBUG
+#define DEBUG
 #define INFO
 
 #ifdef DEBUG
@@ -49,31 +51,52 @@ void info(char *args, ...) {
 void info() { }
 #endif
 
+typedef enum error {
+    NO_ERROR,
+    BAD_ENDING_SEQUENCE,
+    SINGLE_LF_DETECTED,
+    BAD_WHITESPACE,
+    EMPTY_LINE,
+    NUMBER_OVERFLOW,
+    SUM_OVERFLOW,
+    BAD_CHARACTER
+} error_t;
+
 typedef struct buffer {
     char info[BUFFER];
     short amount_read;
     short stop_index;
+    bool space_detected;
+    bool digit_detected;
     bool finished;
 } buffer_t;
 
 typedef struct line {
     char info[LINE_BUFFER];
-    bool error;
+    error_t status;
 } line_t;
 
-typedef enum error {
-    NO_ERROR,
-    BAD_ENDING_SEQUENCE,
-    BAD_WHITESPACE,
-    NUMBER_OVERFLOW,
-    SUM_OVERFLOW
-} error_t;
 
-int counter = 0;
+char* name(error_t error)
+{
+    switch(error)
+    {
+        case NO_ERROR: return "NO_ERROR";
+        case BAD_ENDING_SEQUENCE: return "BAD_ENDING_SEQUENCE";
+        case SINGLE_LF_DETECTED: return "SINGLE_LF_DETECTED";
+        case BAD_WHITESPACE: return "BAD_WHITESPACE";
+        case EMPTY_LINE: return "EMPTY_LINE";
+        case NUMBER_OVERFLOW: return "NUMBER_OVERFLOW";
+        case SUM_OVERFLOW: return "SUM_OVERFLOW";
+        case BAD_CHARACTER: return "BAD_CHARACTER";
+        default: return "BAD_STATE";       
+    }
+}
+
 int server_fd, client_fd;
 bool CR_detected = false;
-buffer_t buffer = {.amount_read = 0, .stop_index = -1, .finished = false};
-line_t line = {.error = false};
+buffer_t buffer = {.amount_read = 0, .stop_index = -1, .finished = false, .space_detected = false, .digit_detected = false};
+line_t line = {.status = NO_ERROR};
 
 void buffer_reset()
 {
@@ -81,17 +104,20 @@ void buffer_reset()
     buffer.amount_read = 0;
     buffer.stop_index = -1;
     buffer.finished = false;
+    buffer.digit_detected = false;
 }
 
 void line_reset()
 {
     memset(line.info, 0, LINE_BUFFER);
-    line.error = false;
+    line.status = NO_ERROR;
 }
 
-void connection_ended()
+void connection_lost(const char* cause)
 {
-    char* mess = "Connection timed out.\r\n";
+    // char* mess = "Connection timed out.\r\n";
+    char* mess = malloc(sizeof(*mess) * 64);
+    sprintf(mess, "Connection %s\r\n", mess == NULL ? "lost." : cause);
     if(write(client_fd, mess, strlen(mess)) == -1) {
         perror("write error\n");
         exit(EXIT_FAILURE);
@@ -100,23 +126,14 @@ void connection_ended()
         perror("close error\n");
         exit(EXIT_FAILURE);
     }
+    free(mess);
 } 
-
-void chunk_in_ascii()
-{
-    int i = 0;
-    debug("\n------------------------------\n");
-    for(i = 0; i<buffer.amount_read; i++) {
-        printf("%c(%d) ", buffer.info[i], buffer.info[i]);
-    }
-    debug("------------------------------\n");
-}
 
 void whole_chunk_in_ascii()
 {
     int i = 0;
-    debug("\n------------------------------\n");
-    debug("Chunk info [%d]\n", counter++);
+    debug("------------------------------\n");
+    debug("Chunk info [index] = CHAR(ASCII)\n");
     for(i = 0; i<BUFFER; i++) {
         if(buffer.info[i] == CR) debug("[%d] = CR\n", i);
         else if(buffer.info[i] == LF) debug("[%d] = LF\n", i); 
@@ -129,16 +146,19 @@ void whole_chunk_in_ascii()
 void line_in_ascii()
 {
     int i = 0;
-    debug("\n------------------------------\n");
-    debug("Line info\n");
+    debug("------------------------------\n");
+    debug("Line info - [index] = CHAR(ASCII)\n");
     for(i = 0; i<strlen(line.info); i++) {
         if(line.info[i] == CR) debug("[%d] = CR\n", i);
         else if(line.info[i] == LF) debug("[%d] = LF\n", i); 
         else debug("[%d] = %c(%d)\n", i, line.info[i], line.info[i]);
     }
-    debug("error=%d\n", line.error);
+    debug("error=%s\n", name(line.status));
     debug("------------------------------\n");
 }
+
+bool err_check() { return line.status != NO_ERROR; }
+void err_set(error_t error) { if(line.status == NO_ERROR) line.status = error; }
 
 //signalize if the read was interrupted
 bool line_read()
@@ -146,25 +166,17 @@ bool line_read()
     int i = 0;
     bool interrupted = false;
 
-    // printf("CHUNK:\n%s", buffer.info);
-    // printf("CHUNK: ");
-    // chunk_in_ascii();
-    // if(buffer.stop_index != -1) {
-    //     strncpy(buffer.info, buffer.info+buffer.stop_index+1, buffer.amount_read);
-    //     buffer.stop_index++;
-    //     condition = buffer.amount_read-buffer.stop_index;
-    //     buffer.stop_index = 0;
-    //     whole_chunk_in_ascii();
-    //     // i = buffer.stop_index+1;
-    // }
-
     for(; i < buffer.amount_read; i++) {
         if(buffer.info[i] == CR) {
             CR_detected = true;
-            debug("CR DETECTED\n");
+            debug("CR DETECTED ON [%d]\n", i);
+            if(buffer.space_detected) {
+                debug("ERROR OCCURED ON LINE 171\n");
+                line.status = BAD_WHITESPACE;
+            }
         } 
         else if(buffer.info[i] == LF) {
-            debug("LF DETECTED\n");
+            debug("LF DETECTED ON [%d]\n", i);
             if(CR_detected) {
                 buffer.stop_index = i;
                 CR_detected = false;
@@ -172,12 +184,32 @@ bool line_read()
                 break;
             } else {
                 CR_detected = false;
-                line.error = true;
+                debug("ERROR OCCURED ON LINE 185\n");
+                line.status = SINGLE_LF_DETECTED;
             }
         } else {
             if(CR_detected) {
                 CR_detected = false;
-                line.error = true;
+                line.status = BAD_ENDING_SEQUENCE;
+                buffer.space_detected = false;
+                debug("ERROR OCCURED ON LINE 193\n");
+            } else if(!isdigit(buffer.info[i])) {
+                if(buffer.info[i] == SPACE) {
+                    if(!buffer.space_detected) {
+                        buffer.space_detected = true;
+                    } else {
+                        line.status = BAD_WHITESPACE;
+                        debug("ERROR OCCURED ON LINE 198\n");  
+                    }
+                } else {
+                    debug("NON-DIGIT FOUND ON [%d]\n", i);
+                    line.status = BAD_CHARACTER;
+                    buffer.space_detected = false;
+                    debug("ERROR OCCURED ON LINE 202");
+                } 
+            } else {
+                buffer.space_detected = false;
+                buffer.digit_detected = true;
             }
         }
     }
@@ -200,11 +232,13 @@ bool will_overflow(ul a, ul b) { return a > ULONG_MAX - b; }
 void safe_add(char* number, ul* sum)
 {
     ul num = strtoul(number, NULL, 10);
-    if(errno == ERANGE)
-        line.error = true;
+    if(errno == ERANGE) {
+        line.status = NUMBER_OVERFLOW;
+        errno = 0;
+    }
     else {
         if(will_overflow(num, *sum))
-            line.error = true;
+            line.status = SUM_OVERFLOW;
         else
             *sum += num;
     }
@@ -213,13 +247,21 @@ void safe_add(char* number, ul* sum)
 void line_interpret()
 {
     ul sum = 0;
-    char* number = strtok(line.info, " ");
-    while(number) {
-        debug("NUMBER = %s\n", number);
-        safe_add(number, &sum);
-        number = strtok(NULL, " ");
+    if(buffer.digit_detected) {
+        if(line.info[0] == SPACE)
+            line.status = BAD_WHITESPACE;
+        else {
+            char* number = strtok(line.info, " ");
+            while(number) {
+                debug("NUMBER = %s\n", number);
+                safe_add(number, &sum);
+                number = strtok(NULL, " ");
+            }
+        }
+    } else {
+        line.status = EMPTY_LINE;  
     }
-    line.error ? sprintf(line.info, "%s\r\n", ERROR_MESSAGE) : sprintf(line.info, "%lu\r\n", sum);
+    err_check() ? sprintf(line.info, "%s\r\n", ERROR_MESSAGE) : sprintf(line.info, "%lu\r\n", sum);
 }
 
 void set_timeout(int seconds, int microseconds)
@@ -275,16 +317,17 @@ int main(int argc, char* argv[])
         {
             if(buffer.amount_read == -1) break; //timeout
             info("READ \033[1;32m%d\033[0m BYTES OF DATA\n", buffer.amount_read);
-            whole_chunk_in_ascii();
+            //whole_chunk_in_ascii();
             if(line_read()) { 
                 debug("LINE = %s\n", line.info);
                 line_in_ascii();
-                whole_chunk_in_ascii();
+                //whole_chunk_in_ascii();
 
                 line_interpret();
+                if(err_check()) info("--\033[1;33m%s\033[0m OCCURED--\n", name(line.status));
                 write(client_fd, line.info, strlen(line.info));
-                if(line.error) info("--AN ERROR OCCURED--\n");
                 info("LINE SENT\n\n");
+                line_reset();
 
                 memset(line.info, 0, LINE_BUFFER);
                 debug("FINISHED = %d\n", buffer.finished);
@@ -292,14 +335,14 @@ int main(int argc, char* argv[])
                     debug("Inside while loop\n");
                     if(line_read()) {
                         debug("LINE = %s\n", line.info);                
-                        line_in_ascii();
+                        //line_in_ascii();
                         line_interpret();
                         info("LINE SENT\n");
                         write(client_fd, line.info, strlen(line.info));
                         line_reset();
 
                     } else {
-                        line_in_ascii();
+                        //line_in_ascii();
                     }
                 }
             } else {
@@ -307,6 +350,10 @@ int main(int argc, char* argv[])
             }
         }
         
-        connection_ended();
+        if(buffer.amount_read == -1) {
+            connection_lost("timed out");
+        } else {
+            connection_lost(NULL);
+        }
     }
 }
--- z5/src/test.c
+++ z5/src/test.c
@@ -3,9 +3,13 @@
 
 int main()
 {
-    char test[128] = "1 2  3 4 5 6 7";
+    char test[128] = " \r\n";
+    printf("%ld\n", strlen(test));
+    int xd = 0;
     char *number = strtok(test, " ");
     while(number) {
+        printf("%d\n", xd);
+        xd++;
         printf("number = %s\n", number);
         number = strtok(NULL, " ");
     }
